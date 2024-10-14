f99e62dce699addc2fd7006345271147167cf81b
Xianghao Wang <u7321615@anu.edu.au>
2024-10-15 01:28:50 +1100

    remove model's jacobian getter

--- cugpufit/model.py
+++ cugpufit/model.py
@@ -4,16 +4,9 @@ class Model(ABC):
     def __init__(self, model_name, estimator, dtype):
         self.model_name = model_name
         self.estimator = estimator
-        self._jacobian = None
+        self.jacobian = None
         self.dtype = dtype
 
     @abstractmethod
     def predict(self, inputs, train):
         pass
-
-    @property
-    def jacobian(self):
-        if self._jacobian == None:
-            raise ValueError('Please call predict() with train before querying jacobian.')
-
-        return self._jacobian
