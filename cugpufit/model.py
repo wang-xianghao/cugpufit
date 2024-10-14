697ed82dcd9eea199c7c674afa430209e666fe18
Xianghao Wang <u7321615@anu.edu.au>
2024-10-15 01:01:34 +1100

    add data type option to model

--- cugpufit/model.py
+++ cugpufit/model.py
@@ -4,21 +4,14 @@ class Model(ABC):
     def __init__(self, model_name, estimator, dtype):
         self.model_name = model_name
         self.estimator = estimator
-        self.dtype = dtype
         self._jacobian = None
+        self.dtype = dtype
 
     @abstractmethod
     def predict(self, inputs, train):
         pass
 
     @property
-    def jacobian(self):
-        if self._jacobian == None:
-            raise ValueError('Please call predict() with train before querying jacobian.')
-
-        return self._jacobian
-    
-    @residuals
     def jacobian(self):
         if self._jacobian == None:
             raise ValueError('Please call predict() with train before querying jacobian.')
