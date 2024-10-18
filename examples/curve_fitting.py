fecefaea6a5c08a7bba7c5c7a32f7a70f8f36cac
Xianghao Wang <u7321615@anu.edu.au>
2024-10-18 22:51:26 +1100

    add jacobian to curve_fitting example

--- examples/curve_fitting.py
+++ examples/curve_fitting.py
@@ -1,15 +1,19 @@
 import cunumeric as np
+# import numpy as np
+
+from itertools import accumulate
 
 from cugpufit.model import Model
 from cugpufit.losses.mse import MSE
 
+# TODO: data types in the model should be consistent
 class SimpleDense(Model):
     def __init__(self, estimator, dtype):
         super().__init__('curve fitting', estimator, dtype)
 
         def glorot_uniform(shape, k):
             limit = np.sqrt(6 / k)
-            return np.random.uniform(-limit, limit, shape, dtype=self.dtype)
+            return np.random.uniform(-limit, limit, shape)
 
         self.W1 = glorot_uniform((1, 20), 21)
         self.b1 = np.zeros(20, dtype=self.dtype)
@@ -17,6 +21,8 @@ class SimpleDense(Model):
         self.b2 = np.zeros(1, dtype=self.dtype)
 
         self.parameters = [self.W1, self.b1, self.W2, self.b2]
+        self.n_parameters = sum(map(np.size, self.parameters))
+        self.offsets = list(accumulate(map(np.size, self.parameters)))
     
     def predict(self, inputs, train=False):
         N, D = inputs.shape
@@ -28,8 +34,15 @@ class SimpleDense(Model):
         if not train:
             return y2
 
-        # TODO: Calculate jacobian by manual backpropagation
-        self.jacobian = None
+        # Calculate jacobian manually
+        # TODO: use autograd tools
+        self.jacobian = np.empty((N, self.n_parameters), dtype=model.dtype)
+        
+        for i in range(N):
+            self.jacobian[i:i+1, self.offsets[0]:self.offsets[1]] = y1[i, :] * self.W2.T
+            self.jacobian[i:i+1, 0:self.offsets[0]] = np.dot(inputs[i, :], self.jacobian[i:i+1, self.offsets[0]:self.offsets[1]])
+            self.jacobian[i:i+1, self.offsets[1]:self.offsets[2]] = y1[i, :]
+            self.jacobian[i:i+1, self.offsets[2]:self.offsets[3]] = 1.0
 
         return y2
 
