9631a3c43e21b9fe379012b850b4e9ec8e65ed7c
Xianghao Wang <u7321615@anu.edu.au>
2024-10-20 14:06:01 +1100

    clean all the codes

--- cugpufit/losses/loss.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Loss(ABC):
-    @abstractmethod
-    def loss_with_residuals(self, outputs, targets):
-        pass
--- cugpufit/losses/mse.py
+++
@@ -1,9 +0,0 @@
-import cunumeric as np
-
-from .loss import Loss
-
-class MSE(Loss):
-    def loss_with_residuals(self, outputs, targets):
-        residuals = targets - outputs
-
-        return np.mean(np.square(residuals)), residuals
